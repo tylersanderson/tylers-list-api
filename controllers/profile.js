86e07b3fb010f6f36b7e40f4ae446c5a1d6ba4d2
tylersanderson <tylersanderson@hotmail.com>
2020-07-05 08:17:33 -0500

    added new profile endpoint to get name of gig poster

--- controllers/profile.js
+++ controllers/profile.js
@@ -11,6 +11,19 @@ const handleProfileGet = (req, res, db) => {
 	.catch(err => res.status(400).json('error getting user'))
 }
 
+const handleProfileNameGet = (req, res, db) => {
+	const { id } = req.params;
+	db.select('name').from('users').where({id})
+		.then(user => {
+			if(user.length) {
+				res.json(user[0])
+			} else {
+				res.status(400).json('Not found')
+			}
+	})
+	.catch(err => res.status(400).json('error getting user'))
+}
+
 const handleProfileUpdate = (req, res, db) => {
 	const { id } = req.params;
 	const { name, age, occupation } = req.body.formInput;
@@ -29,5 +42,6 @@ const handleProfileUpdate = (req, res, db) => {
 
 module.exports = {
 	handleProfileGet,
-	handleProfileUpdate
+	handleProfileUpdate,
+	handleProfileNameGet
 }
