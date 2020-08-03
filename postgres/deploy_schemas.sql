1b23333176ca803cb49a1c467160cef2f36724f4
tylersanderson <tylersanderson@hotmail.com>
2020-08-03 15:58:34 -0500

    removing leftover code

--- postgres/deploy_schemas.sql
+++ postgres/deploy_schemas.sql
@@ -1,7 +1,6 @@
 -- Deploy fresh database tables
 \i '/docker-entrypoint-initdb.d/tables/users.sql'
 \i '/docker-entrypoint-initdb.d/tables/login.sql'
-\i '/docker-entrypoint-initdb.d/tables/orders.sql'
 \i '/docker-entrypoint-initdb.d/tables/gigs.sql'
 
 \i '/docker-entrypoint-initdb.d/seed/seed.sql'
